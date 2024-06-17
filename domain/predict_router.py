27f0af2f484eb8425bd0a2060a92d03619df2537
unknown <isakin.tyrfing@gmail.com>
2024-06-17 17:59:46 +0900

    [Refactor] modify predict api

--- domain/predict_router.py
+++ domain/predict_router.py
@@ -4,7 +4,6 @@ import os
 from typing import Dict
 
 from lib.model_manager import get_models
-from lib.image_controll import combine_images_grid
 from lib.logger_config import setup_logger
 from domain.predict_service import predict, save_uploaded_file
 from domain.predict_schema import PredictionRequest
@@ -15,7 +14,7 @@ router = APIRouter()
 @router.post("/json")
 async def upload_json(request: PredictionRequest, models: Dict[str, YOLO] = Depends(get_models)):
     result = await predict(models, request, save=True)
-    return result
+    return result['result_images']
 
 @router.post("/image")
 async def upload_image(file: UploadFile = File(...), models: Dict[str, YOLO] = Depends(get_models)):
@@ -25,26 +24,6 @@ async def upload_image(file: UploadFile = File(...), models: Dict[str, YOLO] = D
     request = PredictionRequest(**data_dict)
     
     result = await predict(models, request, save=True)
+
     os.remove(file_path)
-    return result
-
-@router.post("/test/all-models")
-async def upload_image(file: UploadFile = File(...), models: Dict[str, YOLO] = Depends(get_models)):
-    # file_path = await save_uploaded_file(file)
-    file_path = [
-        'KakaoTalk_20240611_133910242_01.jpg',
-        'KakaoTalk_20240611_133910242_02.jpg',
-        'KakaoTalk_20240611_133910242_03.jpg',
-        'KakaoTalk_20240611_133910242.jpg'
-    ]
-
-    results = []
-    for model in models:
-        data_dict = {"modelName": model, "images": file_path}
-        request = PredictionRequest(**data_dict)
-        result = await predict(models, request, save=True)
-        results.extend(result['result_images'])
-
-        combine_images_grid(result['result_images'], f'{model}.jpg')
-    os.remove(file_path)
-    return results
+    return result['result_images']
--- domain/predict_service.py
+++ domain/predict_service.py
@@ -1,12 +1,15 @@
 from ultralytics import YOLO
 from fastapi import UploadFile
-from typing import Dict
+from typing import Dict, List
 import os
+import requests
+from ultralytics.engine.results import Results
 
 from lib.const import DEFAULT_MODEL, RESULT_SAVE_DIR, UPLOAD_DIR
 from lib.logger_config import setup_logger
 from lib.image_controll import predict_summary
 from domain.predict_schema import PredictionRequest
+
 
 logger = setup_logger()
 
@@ -14,10 +17,16 @@ async def predict(models: Dict[str, YOLO], request: PredictionRequest, save=Fals
     model_name = request.modelName if request.modelName else DEFAULT_MODEL
     model = models.get(model_name)
 
+    images = download_images(request.images)
+
     if not model:
         raise ValueError(f"Model {model_name} not found in models dictionary")
-
-    results = model.predict(request.images, conf=0.25, verbose=False)
+    
+    results: List[Results] = []
+    for image in images:
+        result = model.predict(image, conf=0.25, verbose=False)
+        results.append(result[0])
+    
     logger.info(f"📌 Prediction results - {predict_summary(results, model_name)}")
 
     # Save the results
@@ -26,7 +35,7 @@ async def predict(models: Dict[str, YOLO], request: PredictionRequest, save=Fals
         save_dir = RESULT_SAVE_DIR
         os.makedirs(save_dir, exist_ok=True)
         for result in results:
-            image_path = os.path.join(save_dir, f"{os.path.splitext(model_name)[0]}_{os.path.basename(result.path)}")
+            image_path = os.path.join(save_dir, f"{os.path.basename(result.path)}.jpg")
             result.save(filename=image_path)
             result_images.append(image_path)
 
@@ -40,3 +49,24 @@ async def save_uploaded_file(uploaded_file: UploadFile) -> str:
     with open(file_path, "wb") as buffer:
         buffer.write(await uploaded_file.read())
     return file_path
+
+def download_images(urls: List[str]):
+    save_dir = UPLOAD_DIR
+    os.makedirs(save_dir, exist_ok=True)
+
+    images = []
+    for url in urls:
+        name = url.split("/")[-1]
+        save_path = os.path.join(save_dir, name)
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+
+            with open(save_path, 'wb') as f:
+                f.write(response.content)
+
+            images.append(save_path)
+        except Exception as e:
+            logger.error(f"Failed to download image: {e}")
+    
+    return images
